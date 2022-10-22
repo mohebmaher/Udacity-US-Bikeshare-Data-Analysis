3dca7686ed4a2973106811f6031d1e139aa62abb
moheb_maher <dvm.moheb.maher@gmail.com>
2022-10-22 14:56:12 +0200

    fix: fix some bugs in us_bikeshare_optimized.py

--- us_bikeshare_optimized.py
+++ us_bikeshare_optimized.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import datetime as dt
 
-# Declare a CONSTANT variable with path to each dataset
-CITIES = {'Chicago': 'chicago.csv',
-          'New York': 'new_york_city.csv',
-          'Washington': 'washington.csv'}
+# Declare some CONSTANTS for validating user inputs
+CITIES = {'Chicago': './data/chicago.csv',
+          'New York': './data/new_york_city.csv',
+          'Washington': './data/washington.csv'}
+
+DAYS = ['Saturday', 'Sunday', 'Monday', 'Tuesday',
+        'Wednesday', 'Thursday', 'Friday']
+
+MONTHS = ['January', 'February', 'March', 'April',
+          'May', 'June', 'July', 'August',
+          'September', 'October', 'November', 'December']
 
 
 def load_data():
@@ -20,11 +27,11 @@ def load_data():
     """
 
     # Print some user instructions
-    print("Please, choose on of the following datasets:")
+    print("\nPlease, choose on of the following datasets:")
     print("  1. Chicago,")
     print("  2. New York,")
     print("  3. Washington.")
-    print("NOTE: you can quit by pressing enter.")
+    print("NOTE: you can quit by pressing <enter>.")
 
     while True:
         # Get the user input
@@ -36,8 +43,8 @@ def load_data():
             return None
         elif user_input in CITIES:
             print("\nLoading data..")
-            path = f"./data/{CITIES[user_input]}"
-            loaded_data = pd.read_csv(path, parse_dates=['Start Time', 'End Time']).iloc[:, 1:]
+            loaded_data = pd.read_csv(filepath_or_buffer=f"{CITIES[user_input]}",
+                                      parse_dates=['Start Time', 'End Time']).iloc[:, 1:]
             print("Done!")
             return loaded_data
         else:
@@ -64,7 +71,7 @@ def get_data_ready(raw_data):
     raw_data.rename(columns=lambda x: x.replace(' ', '_').lower(), inplace=True)
 
     # Reset data type for birth_year
-    if raw_data.get('birth_year'):
+    if 'birth_year' in raw_data.columns:
         raw_data['birth_year'] = raw_data['birth_year'].astype(int)
 
     # Create new columns as needed
@@ -93,11 +100,12 @@ def explore_data(data):
 
     # Print some user instructions
     print('\nYou can explore data by viewing a defined numer of rows at a time.')
+    print("NOTE: you can quit by pressing <enter>.")
 
     i = 0
     while True:
         # Get the user input
-        rows = input('Number of rows (to skip press enter): ').strip()
+        rows = input('\nNumber of rows: ').strip()
 
         # Validate the user input
         if not rows:
@@ -123,10 +131,6 @@ def filter_data(data):
         data (pd.DataFrame): data after being filtered.
     """
 
-    # Create validation lists
-    months = data['month'].unique()
-    days = data['day'].unique()
-
     # Print some user instructions
     print("\nYou can filter the data either by day or month,")
     print("  1. Weekday: Saturday - Friday.")
@@ -136,8 +140,8 @@ def filter_data(data):
 
     while True:
         # Get the user filters
-        user_filters = input("Filter by: ").strip().title().split(' ')
-        filter_by = [f for f in user_filters if f in (*days, *months)]
+        user_filters = input("\nFilter by: ").strip().title().split(' ')
+        filter_by = [f for f in user_filters if f in (*DAYS, *MONTHS)]
 
         # Validate the user input
         if len(user_filters) == 1 and not user_filters[0]:
@@ -170,7 +174,7 @@ def time_stats(data):
 
     # The most common month
     most_common_month = data['month'].value_counts(ascending=False).index[0]
-    print(f"\nMost common travel month: {most_common_month},")
+    print(f"Most common travel month: {most_common_month},")
 
     # The most common month
     most_common_day = data['day'].value_counts(ascending=False).index[0]
@@ -203,7 +207,7 @@ def station_stats(data):
 
     # The most common start station
     most_common_start = data['start_station'].value_counts(ascending=False).index[0]
-    print(f"\nMost common start-end combination: {most_common_start}.")
+    print(f"Most common start-end combination: {most_common_start}.")
 
     # The most common end station
     most_common_end = data['end_station'].value_counts(ascending=False).index[0]
@@ -236,7 +240,7 @@ def trip_duration_stats(data):
 
     # The mean travel time
     mean_travel_time = data['travel_time'].mean()
-    print(f"\nMean travel time: {round(mean_travel_time, 2)} minutes.")
+    print(f"Mean travel time: {round(mean_travel_time, 2)} minutes.")
 
     # The total travel time
     total_travel_time = data['travel_time'].sum()
@@ -266,12 +270,12 @@ def user_stats(data):
     # The counts of user type
     subscribers = len(data.query("user_type == 'Subscriber'"))
     customers = len(data.query("user_type == 'Customer'"))
-    print(f"\nCounts of user types:")
+    print(f"Counts of user types:")
     print(f"  1. Subscribers: {subscribers},")
     print(f"  2. Customers: {customers}.")
 
     # The counts of gender
-    if data.get('gender'):
+    if 'gender' in data.columns:
         males = len(data.query("gender == 'Male'"))
         females = len(data.query("gender == 'Female'"))
         print(f"\nCounts of user genders:")
@@ -281,7 +285,7 @@ def user_stats(data):
         print("\nNOTE: this dataset has no information about user genders.")
 
     # The earliest, most recent, and most common year of birth
-    if data.get('birth_year'):
+    if 'birth_year' in data.columns:
         earliest_birth_year = data['birth_year'].min()
         print(f"\nEarliest birth year: {earliest_birth_year},")
 
@@ -303,7 +307,8 @@ def main():
 
     """Executes the script."""
 
-    while True:
+    outer_loop = True
+    while outer_loop:
         # Load the data
         city_raw_data = load_data()
 
@@ -321,22 +326,35 @@ def main():
         city_data_filtered = filter_data(city_data)
 
         # View time stats
+        print('*' * 20)
         time_stats(city_data_filtered)
 
         # View station stats
+        print('*' * 20)
         station_stats(city_data_filtered)
 
         # View trip duration stats
+        print('*' * 20)
         trip_duration_stats(city_data_filtered)
 
         # View user stats
+        print('*' * 20)
         user_stats(city_data_filtered)
+        print('*' * 20)
 
         # Ask if the user wants to repeat the whole process
-        print("'\nWould you like to restart?")
-        to_repeat = input("Yes or No: ").strip().lower()
-        if to_repeat == 'no':
-            break
+        print("\nYou can proceed to analyzing another dataset if you would like!")
+
+        while True:
+            print("\nPress any key to restart, or you can quit by pressing <enter>.")
+            to_repeat = input('Restart? ').strip().lower()
+            if to_repeat == '':
+                print("\nThank you!")
+                outer_loop = False
+                break
+            else:
+                print('\nRestarting..')
+                break
 
 
 if __name__ == "__main__":
